f999add6e60ea888d2fe39edcca788aa2105a2e5
Damyan03 <dimi0018@hz.nl>
2024-08-14 19:24:52 +0300

    modified: finished simple line draw function

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,34 +1,37 @@
 import { useEffect, useRef, useState } from "react";
-import { createElement } from "react"
-
 
 function Playground() {
+    const [svgs, setSvgs] = useState([]);
+    const [currentLine, setCurrentLine] = useState(null);
 
-    const [svgs, setSvgs] = useState([])
-    const [svg, setSvg] = useState(null)
+    function handleMouseDown(e) {
+        const bounds = e.currentTarget.getBoundingClientRect();
+        const x1 = e.clientX - bounds.left;
+        const y1 = e.clientY - bounds.top;
+        
+        setCurrentLine({ x1, y1, x2: x1, y2: y1 });
+    }
 
-    function handleAddSvg(e){
-        console.log('drawing line')
-        const bounds = e.target.getBoundingClientRect();
-        let x1 = e.clientX - bounds.left;
-        let y1 = e.clientY - bounds.top;
-        console.log(x1, y1)
-        const newSvg = createElement('line', {height: 300, width: 300, x1: `${x1}`, y1: `${y1}`, x2: '300', y2: '300', stroke: 'red', strokeWidth: 2});
-        setSvgs([...svgs, newSvg])
+    function handleMouseMove(e) {
+        if (!currentLine) return;
+
+        const bounds = e.currentTarget.getBoundingClientRect();
+        const x2 = e.clientX - bounds.left;
+        const y2 = e.clientY - bounds.top;
+        
+        setCurrentLine((cl) => ({...cl, x2, y2}));
     }
-    
-    function drawOngoingSvg(e){
-        console.log('ongoing')
-        if (isDrawing){
-            x2.current = e.clientX - bounds.left;
-            y2.current = e.clientY - bounds.top;
-        }
+
+    function handleMouseUp(e) {
+        if (!currentLine) return;
+
+        setSvgs([...svgs, currentLine]);
+        setCurrentLine(null);
     }
-    
 
     return (
         <div className="center flex-col h-auto w-full bg-gray-300">
-            <div className='flex justify-between items-start w-full'>
+            <div className="flex justify-between items-start w-full">
                 <div>
                     <h1 className="text-3xl">Playground</h1>
                     <p>Let's draw some geometric figures!</p>
@@ -36,18 +39,41 @@ function Playground() {
                 <h1>test</h1>
             </div>
             <div className="bg-green-300 w-full p-5">
-                <svg id="pgcanvas" height="600" width="600" className="bg-white select-none" onMouseDown={handleAddSvg} xmlns="http://www.w3.org/2000/svg">
-                    {svgs.map((svg, index) =>
-                        <svg.type key={index} style={svg.props} 
-                        x1={svg.props.x1}
-                        y1={svg.props.y1}
-                        x2={svg.props.x2}
-                        y2={svg.props.y2} />
+                <svg
+                    id="pgcanvas"
+                    height="600"
+                    width="600"
+                    className="bg-white select-none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={handleMouseMove}
+                    onMouseUp={handleMouseUp}
+                >
+                    {svgs.map((line, index) => (
+                        <line
+                            key={index}
+                            x1={line.x1}
+                            y1={line.y1}
+                            x2={line.x2}
+                            y2={line.y2}
+                            stroke="red"
+                            strokeWidth="2"
+                        />
+                    ))}
+                    {currentLine && (
+                        <line
+                            x1={currentLine.x1}
+                            y1={currentLine.y1}
+                            x2={currentLine.x2}
+                            y2={currentLine.y2}
+                            stroke="red"
+                            strokeWidth="2"
+                        />
                     )}
                 </svg>
             </div>
         </div>
-    )
+    );
 }
 
-export default Playground
+export default Playground;
